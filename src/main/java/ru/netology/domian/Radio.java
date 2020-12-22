782d42d65a5e8463f25af082d69803ffb0c83298
Aleksandr Osipov <eltisprim@gmail.com>
2020-12-22 22:11:51 +0300

    Updated maven.yml

---
+++ src/main/java/ru/netology/domian/Radio.java
@@ -0,0 +1,13 @@
+package ru.netology.domian;
+
+public class Radio {
+    String name;
+    int minVolume;
+    int maxVolume = 10;
+    int currentVolume;
+    int minRadioStation;
+    int maxRadioStation = 9;
+    int currentRadioStation;
+    boolean on;
+
+}
